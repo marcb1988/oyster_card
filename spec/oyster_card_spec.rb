f89bc15308dde2ffad88989664a0f3df296a8cca
Marc Blenkinsop <marcblenky@hotmail.com>
2019-10-21 14:07:30 +0100

    wrote first tests and began coding

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,2 +1,13 @@
-describe OysterCard do
+require "oyster_card"
+
+describe Oystercard do
+  let(:oystercard) { Oystercard.new }
+
+  it "creates and instance of Oystercard" do
+    expect(oystercard).to be_instance_of Oystercard
+  end
+
+  it "has a default balance of 0" do
+    expect (oystercard.balance).to eq 0
+  end
 end
